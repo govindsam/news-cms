d6b68abd232a0895df35a775761cbff3de8d14d1
govind <govindkumarsam@gmail.com>
2023-08-27 08:01:10 +0530

    news section

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 use App\Controllers\Pages;
+use App\Controllers\News; // Add this line
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+
 
 
 $routes->get('/', 'Home::index');
